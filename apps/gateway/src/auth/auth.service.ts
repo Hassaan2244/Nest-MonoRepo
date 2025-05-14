d9a6c26c2a53ce9f1949b35f62f71f682985ce53
Hassaan Baig <hassaanbaig192@gmail.com>
2025-05-14 20:49:14 +0500

    Errors Message added

--- apps/gateway/src/auth/auth.service.ts
+++ apps/gateway/src/auth/auth.service.ts
@@ -2,18 +2,27 @@ import { Inject, Injectable } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { CreateUserDto } from 'common/dtos/create-user.dto';
 import { firstValueFrom } from 'rxjs';
+import { BadRequestException } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
   constructor(@Inject('AUTH_SERVICE') private readonly client: ClientProxy) {}
 
-  async registerUser(dto: CreateUserDto) {
+ async registerUser(dto: CreateUserDto) {
+  try {
     return await firstValueFrom(this.client.send('register_user', dto));
+  } catch (error) {
+    throw new BadRequestException(error.message || 'Registration failed');
   }
+}
 
-  async loginUser(email: string, password: string) {
+async loginUser(email: string, password: string) {
+  try {
     return await firstValueFrom(this.client.send('login_user', { email, password }));
+  } catch (error) {
+    throw new BadRequestException(error.message || 'Login failed');
   }
+}
 
   async getAllUsers() {
     return await firstValueFrom(this.client.send('get_users', {}));
